b6b1550983f4b739d18c12e767bd1fc43f01b23e
Yedija Lewi Suryadi <yedijalewisuryadi@gmail.com>
2023-12-02 08:00:18 +0700

    Add: Modifikasi modul12a.c insertion sort fitur ASC & DESC

---
+++ modul12aModif.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAX 100 //ukuran max array
+
+//mengisi data, data = array data, size = ukuran array
+void IsiData(int data[], int *size){
+    printf("Masukan ukuran array (max 100): ");
+    scanf("%d", size);
+    for(int i = 0; i<*size;i++){
+        printf("Masukan data ke-%d : ", i+1); //i+1 karena indeks dimulai dari 0
+        scanf("%d", &data[i]);
+    }
+}
+
+//Tampilkan data, data = array data, size = ukuran array
+void TampilData(int data[], int size){
+    for (int i=0;i<size;i++)
+        printf("%d ", data[i]);
+    puts("");
+}
+
+//Insertion Sort Asc, arr = array, n = ukuran array , mode=pengurutan 1.menaik/2.menurun
+void InsertionSort(int arr[], int n, int mode){
+    int i, key, j; //i dan j untuk iterasi, key untuk variabel penyimpanan sementara
+    for(i=1; i<n; i++){
+        key = arr[i]; //key dimulai dari indeks 1
+        j = i-1; //yg mau dibandingkan dimulai dari angka sebelum key
+        if (mode==1){
+            while (j>=0 && arr[j]>key){
+                arr[j+1]=arr[j]; //data dimajukan ke depan
+                j=j-1; //indeksnya mundur
+            }
+        }
+        else if (mode==2){
+            while (j>=0 && arr[j]<key){
+                arr[j+1]=arr[j]; //data dimajukan ke depan
+                j=j-1; //indeksnya mundur
+            }
+        }
+        arr[j+1] = key; //nilai yg ditukar adalah j+1!
+    }
+}
+
+void main(){
+    system("cls");
+    int data[MAX]; //inisialisasi array data
+    int size; //ukuran array yg dipakai
+    int Pilihan; //mode pengurutan, menaik/menurun
+    puts("=== PROGRAM INSERTION SORT ===\n");
+    IsiData(data, &size);
+    puts("\nPilih pengurutan: ");
+    puts("1. Menaik (Ascending)");
+    puts("2. Menurun (Descending)");
+    printf("Pilihan : ");
+    scanf("%d", &Pilihan);
+    printf("\nData sebelum diurutkan: ");
+    TampilData(data, size);
+    InsertionSort(data, size, Pilihan);
+    printf("Data setelah diurutkan: ");
+    TampilData(data, size);
+}
